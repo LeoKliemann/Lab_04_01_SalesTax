f6a068184ea5a03d9958cb9db700f9156b63aff9
LeoKliemann <lgkliemann05@gmail.com>
2026-02-06 15:55:50 -0500

    Statements completed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main{
     public static void main(String[] args)
     {
+        //Declarations
         double salesTax = .05;
         double priceOfPurchase = 9.99;
         double taxTotal;
         double priceAfterTax;
 
+        //Statements
+        taxTotal = priceOfPurchase * salesTax;
+        priceAfterTax = taxTotal + priceOfPurchase;
+        System.out.println("The price of your purchase after tax is: $" + priceAfterTax);
     }
 }
