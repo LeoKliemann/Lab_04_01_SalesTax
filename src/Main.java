4634bc57dc27810630cda7514a25b8b6b1079993
LeoKliemann <lgkliemann05@gmail.com>
2026-02-06 15:52:00 -0500

    Declarations completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main{
     public static void main(String[] args)
     {
-        
+        double salesTax = .05;
+        double priceOfPurchase = 9.99;
+        double taxTotal;
+        double priceAfterTax;
+
     }
 }
